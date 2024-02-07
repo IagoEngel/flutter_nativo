0ef8daccd80345c3f291563a031381da6a275881
Iago Engel Serafim <iagoengel@yahoo.com>
2024-02-06 21:08:58 -0300

    Seção 15: Executando Código Nativo no Android e iOS (Aulas 419 até 423)

---
+++ lib/main.dart
@@ -0,0 +1,99 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Nativo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _a = 0;
+  int _b = 0;
+  int _sum = 0;
+
+  Future<void> _calcSum() async {
+    const channel = MethodChannel('iago.curso.flutter/nativo');
+
+    try {
+      final sum = await channel.invokeMethod('calcSum', {"a": _a, "b": _b});
+      setState(() {
+        _sum = sum;
+      });
+    } on PlatformException {
+      setState(() {
+        _sum = 0;
+      });
+    }
+
+    setState(() {
+      _sum = _a + _b;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Text("Nativo"),
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Soma....$_sum',
+                style: const TextStyle(
+                  fontSize: 30,
+                ),
+              ),
+              TextField(
+                keyboardType: TextInputType.number,
+                onChanged: (value) {
+                  setState(() {
+                    _a = int.tryParse(value) ?? 0;
+                  });
+                },
+              ),
+              TextField(
+                keyboardType: TextInputType.number,
+                onChanged: (value) {
+                  setState(() {
+                    _b = int.tryParse(value) ?? 0;
+                  });
+                },
+              ),
+              TextButton(
+                onPressed: _calcSum,
+                child: const Text('Somar'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
